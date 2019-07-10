b25a5f484bd1f88811e6959d4d57ea0fad96965c
taro.tech <taro.tech@gmail.com>
2019-07-10 12:44:17 +0000

    Add_column_status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_031359) do
+ActiveRecord::Schema.define(version: 2019_07_10_122431) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
