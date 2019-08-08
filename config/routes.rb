4688820c466df292bfc17519eaba857d6923d7ce
taro.tech <taro.tech@gmail.com>
2019-08-08 16:44:48 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to: "tasks#index"
+    
+    get "signup", to: "users#new"
+    resources :users, only: [:index, :show, :new, :create]
     
     resources :tasks
 end
