03efad7fea21b2e4d84ef343995e8cedd09e36eb
taro.tech <taro.tech@gmail.com>
2019-08-10 06:04:59 +0000

    user Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/crate'
+  get 'sessions/destroy'
     root to: "tasks#index"
+    resources :tasks
+
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    delete "logout", to: "sessions#destroy"
     
     get "signup", to: "users#new"
     resources :users, only: [:index, :show, :new, :create]
-    
-    resources :tasks
 end
